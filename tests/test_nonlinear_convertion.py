1a1c3984631008970128cf9b1f3e6ed684606132
zenius <zenius@outlook.com>
2023-10-29 23:18:21 +0800

    tests: effects of standard temperature changing

---
+++ tests/test_nonlinear_convertion.py
@@ -0,0 +1,13 @@
+from math import isclose
+
+from z_units import config
+from z_units import quantity as q
+
+
+def test_convert():
+    config.set_standard_temperature(20)
+    f = q.MolarFlow(1, 'Nm3/h')
+    f1 = f.to('Sm3/h').value
+    config.set_standard_temperature(15)
+    f2 = f.to('Sm3/h').value
+    assert not isclose(f1, f2)
